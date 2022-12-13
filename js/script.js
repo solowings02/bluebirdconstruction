3943e8493b252b9ba97f086b8dbc970bc0169a9b
Amrit Pokharel <pokharelamrit16@gmail.com>
2022-12-13 14:55:27 +0545

    editing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
 let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
