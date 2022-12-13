93385b4707486c5202835f7de5ed655204fc8a4f
Amrit Pokharel <pokharelamrit16@gmail.com>
2022-12-13 15:41:22 +0545

    full-refreshed

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
-let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
-let contactInfo = document.querySelector('.contact-info');
+let navbar = document.querySelector(".header .navbar");
+let contactInfo = document.querySelector(".contact-info");
 
-document.querySelector('#menu-btn').onclick = () =>{
-   navbar.classList.toggle('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
 };
 
+document.querySelector("#info-btn").onclick = () => {
+  contactInfo.classList.add("active");
+};
 
-document.querySelector('#info-btn').onclick = () =>{
-   contactInfo.classList.add('active');
-}
+document.querySelector("#close-contact-info").onclick = () => {
+  contactInfo.classList.remove("active");
+};
 
-document.querySelector('#close-contact-info').onclick = () =>{
-   contactInfo.classList.remove('active');
-}
-
-window.onscroll = () =>{
-   navbar.classList.remove('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
-   contactInfo.classList.remove('active');
-}
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
+  contactInfo.classList.remove("active");
+};
 let swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
@@ -40,8 +37,3 @@ let swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-
-
-
-
